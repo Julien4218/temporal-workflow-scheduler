2d4327564d755ba1f1883d04494a0410125c2b41
Julien <jerard@newrelic.com>
2023-11-18 11:07:20 -0800

    feat: wire newrelic event api

--- workflows/event-workflow.go
+++ workflows/event-workflow.go
@@ -2,6 +2,8 @@ package workflows
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/golang/protobuf/ptypes/timestamp"
@@ -11,6 +13,7 @@ import (
 
 	"github.com/sirupsen/logrus"
 
+	newrelicActivities "github.com/Julien4218/temporal-newrelic-activity/activities"
 	"github.com/Julien4218/temporal-workflow-scheduler/instrumentation"
 )
 
@@ -21,42 +24,46 @@ type EventWorkflowInput struct {
 }
 
 func EventWorkflow(ctx workflow.Context, input *EventWorkflowInput) error {
-	workflowStartTimestamp := *timestamppb.Now()
+	workflowStartTimestamp := timestamppb.Now()
 	workflowStarttime := workflowStartTimestamp.AsTime()
-
 	nowAsMinute := time.Now().Truncate(time.Second).Truncate(time.Millisecond).Truncate(time.Microsecond).Truncate(time.Nanosecond)
 	timeInterval := nowAsMinute.Sub(workflowStarttime)
 
+	ctx = updateWorkflowContextOptions(ctx)
 	logrus.Infof("%s-EventWorkflow started", instrumentation.Hostname)
 	defer logrus.Infof("%s-EventWorkflow completed", instrumentation.Hostname)
 	txn := instrumentation.NrApp.StartTransaction("EventWorkflow")
 	defer txn.End()
 
-	// ctx = updateWorkflowContextOptions(ctx)
 	logrus.Infof(fmt.Sprintf("Got input:%s", input.TimestampStart.String()))
 
-	// eventInput := &newrelicActivities.CreateEventInput{
-	// 	AccountID:     0,
-	// 	EventDataJson: "",
-	// }
+	accountID, err := strconv.Atoi(os.Getenv("NEW_RELIC_ACCOUNT_ID"))
+	if err != nil {
+		logrus.Errorf(fmt.Sprintf("error could not find environment variable NEW_RELIC_ACCOUNT_ID"))
+		return err
+	}
+	eventInput := &newrelicActivities.CreateEventInput{
+		AccountID:     accountID,
+		EventDataJson: fmt.Sprintf("{\"eventType\":\"temporalWorkflowScheduler\", \"timestamp\":\"%s\", \"timeIntervalMs\":\"%s\"}", workflowStarttime.Unix(), timeInterval.Milliseconds()),
+	}
 
-	// var result interface{}
-	// if err := workflow.ExecuteActivity(ctx, newrelicActivities.NewCreateEventActivity, input).Get(ctx, &result); err != nil {
-	// 	logrus.Errorf("Activity NewCreateEventActivity failed. Error: %s", err)
-	// 	return err
-	// }
+	var result interface{}
+	if err := workflow.ExecuteActivity(ctx, newrelicActivities.NewCreateEventActivity, eventInput).Get(ctx, &result); err != nil {
+		logrus.Errorf("Activity NewCreateEventActivity failed. Error: %s", err)
+		return err
+	}
 
 	logrus.Infof("time difference is %s", timeInterval)
 
 	return nil
 }
 
-// func updateWorkflowContextOptions(ctx workflow.Context) workflow.Context {
-// 	// Define the SlackMessageActivity Execution options
-// 	// StartToCloseTimeout or ScheduleToCloseTimeout must be set
-// 	activityOptions := workflow.ActivityOptions{
-// 		StartToCloseTimeout: 10 * time.Second,
-// 	}
-// 	ctx = workflow.WithActivityOptions(ctx, activityOptions)
-// 	return ctx
-// }
+func updateWorkflowContextOptions(ctx workflow.Context) workflow.Context {
+	// Define the SlackMessageActivity Execution options
+	// StartToCloseTimeout or ScheduleToCloseTimeout must be set
+	activityOptions := workflow.ActivityOptions{
+		StartToCloseTimeout: 10 * time.Second,
+	}
+	ctx = workflow.WithActivityOptions(ctx, activityOptions)
+	return ctx
+}
