de5104ad9bbe6e700baab72621f75a83348e2a3f
Julien Erard <jerard@newrelic.com>
2023-11-22 10:31:39 -0800

    chore: update delete to filter on not running

--- deleteAllWorkflows.sh
+++ deleteAllWorkflows.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-temporal workflow delete --query "WorkflowType='EventWorkflow'" --reason "because" -y
+temporal workflow delete --query="ExecutionStatus = 'Running' and WorkflowType='EventWorkflow'" --reason "removing old" -y
